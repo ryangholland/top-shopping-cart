b74c96fce7f4222f5c001e800ec53b00e003f9ac
ryangholland <rghcode@gmail.com>
2024-12-18 10:28:06 -0500

    Style home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Container from "react-bootstrap/Container";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import { Container, Button, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <section className="home-section d-flex align-items-center">
+      <Container className="home-container text-white text-center ">
+        <Stack>
+          <h1 className="pt-4">
+            Welcome to <strong>Tiem</strong>!
+          </h1>
+          <h2>The Item Store</h2>
+          <p className="pt-4">Shop for the best items!</p>
+          <Button variant="light" className="mx-auto" as={Link} to="/store">
+            Shop now
+          </Button>
+        </Stack>
+      </Container>
+    </section>
+  );
 }
 
 export default HomePage;
