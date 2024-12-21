7d6871e0f1d39e4d51f96161276a20023a6613b9
ryangholland <rghcode@gmail.com>
2024-12-21 13:49:13 -0500

    Allow user to change quantities from Item pages

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,14 +1,19 @@
 import { useParams, Link } from "react-router-dom";
 import { useContext } from "react";
-import { formatPrice } from "../utils/utils";
+import { formatPrice, getQuantity } from "../utils/utils";
 import ItemContext from "../context/ItemContext";
 import { FaShoppingCart, FaArrowLeft } from "react-icons/fa";
 import { Container, Row, Col, Image, Button, Spinner } from "react-bootstrap";
 
+import CartContext from "../context/CartContext";
+import Counter from "../components/Item/Counter";
+
 function ItemPage() {
+  const { cart, addToCart } = useContext(CartContext);
   const { items, loading, error } = useContext(ItemContext);
   const { id } = useParams();
   const item = items[id - 1];
+  const quantity = getQuantity(cart, Number(id));
 
   if (loading) {
     return (
@@ -24,9 +29,7 @@ function ItemPage() {
 
   return (
     <section className="item-page-section d-flex flex-column justify-content-center  bg-light">
-      
       <Container className="item-page-container  text-center bg-white w-auto p-4 rounded">
-        
         <Row xs={1} sm={1} md={2} className="g-4">
           <Col className="d-flex justify-content-center align-items-center">
             <Image
@@ -52,19 +55,33 @@ function ItemPage() {
               <p>{item.description}</p>
             </div>
             <div>
-              <h5>Quantity:</h5>
-
-              <p>- 1 +</p>
-              <Button variant="success" className="w-100">
-                <FaShoppingCart className="me-2" />
-                Add to Cart
-              </Button>
+              {!quantity ? (
+                <Button
+                  variant="success"
+                  className="w-100"
+                  onClick={() => addToCart(item)}
+                >
+                  <FaShoppingCart
+                    className="me-2"
+                    onClick={() => addToCart(item)}
+                  />
+                  Add to Cart
+                </Button>
+              ) : (
+                <>
+                
+                <h5>Quantity:</h5>
+                <Counter item={item} />
+                </>
+              )}
             </div>
           </Col>
         </Row>
       </Container>
       <Container className="ps-4 my-3">
-        <Button variant="secondary" as={Link} to={"/store"}><FaArrowLeft /> Back to Store</Button>
+        <Button variant="secondary" as={Link} to={"/store"}>
+          <FaArrowLeft /> Back to Store
+        </Button>
       </Container>
     </section>
   );
